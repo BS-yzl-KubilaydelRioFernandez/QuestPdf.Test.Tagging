3f144697de87cc8c03a879b95ea320fb9a99cfc1
kubi04 <122866531+BS-yzl-KubilaydelRioFernandez@users.noreply.github.com>
2025-09-29 12:57:57 +0200

    pdf/a and pdf/ua conformance support

--- QuestPdf.Test.Tagging/InvoiceDocument.cs
+++ QuestPdf.Test.Tagging/InvoiceDocument.cs
@@ -17,6 +17,8 @@ namespace QuestPdf.Test.Tagging
 
         public DocumentMetadata GetMetadata() => new() { Title = "Invoice Example", Language = "en-US" };
 
+        public DocumentSettings GetSettings() => new() { PDFA_Conformance = PDFA_Conformance.PDFA_3A, PDFUA_Conformance = PDFUA_Conformance.PDFUA_1 };
+
         public void Compose(IDocumentContainer container)
         {
             container.Page(page =>
